5efdd9b2991d1adb61167c341a2a8291c968b31b
Murilo Câmara da Silva <murilocs.sjc@gmail.com>
2024-04-14 18:22:00 -0300

    configurações do gh pages

--- src/sections/ViewProductSection.jsx
+++ src/sections/ViewProductSection.jsx
@@ -8,16 +8,16 @@ const ViewProductSection = ({
 }) => {
   return (
     <section>
-        <div className="flex flex-row justify-between">
+        <div className="flex justify-between items-center max-2xl:flex-col gap-10 w-full max-container">
             <img 
                 src={imgURL}
                 alt="shoes" 
-                className="w-[50%]"
+                className="w-[50%] max-xl:w-[50%]"
             />
-            <div className="flex flex-col w-[30%] items-center gap-4">
+            <div className="flex flex-col w-[30%] items-center gap-4 max-xl:w-[50%]">
                 <h3 className="text-2xl">{name}</h3>
                 <h2 className="text-3xl font-bold text-coral-red font-montserrat">{price}</h2>
-                <p className="font-montserrat text-slate-gray text-justify info-text text-1xl">There's a reason why MJ's fifth sneaker remains a fan favorite. Its fierce details on the zig-zag midsole, iconic lace locks, and mesh-inspired shoe cage are instantly recognizable, providing a striking style that you can rock any day of the week.</p>
+                <p className="font-montserrat text-slate-gray text-justify info-text text-1xl max-sm:text-xs">There's a reason why MJ's fifth sneaker remains a fan favorite. Its fierce details on the zig-zag midsole, iconic lace locks, and mesh-inspired shoe cage are instantly recognizable, providing a striking style that you can rock any day of the week.</p>
                 <Button
                     label="Order Now"
                     backgroundColor="bg-coral-red"
